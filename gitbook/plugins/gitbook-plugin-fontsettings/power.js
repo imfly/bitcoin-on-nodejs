ae85a505152a7cd6448515feae7c9466b0debe70
imfly <kubying@foxmail.com>
2016-03-03 16:34:09 +0800

    Update 2016-03-03T08:33:31.799Z

--- gitbook/plugins/gitbook-plugin-fontsettings/power.js
+++
@@ -1,7 +0,0 @@
-require(["gitbook", "lodash", "jQuery"], function(gitbook, _, $) {
-    gitbook.events.bind("page.change", function() {
-        var $link = $(".gitbook-link")[0];
-        $link.text = "撰写并维护: @imfly";
-        $link.href = "http://book.btcnodejs.com"
-    });
-});
