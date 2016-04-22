6701869411c0977176239ced0fabf83ce8c7cda3
imfly <kubying@foxmail.com>
2016-04-22 13:45:54 +0800

    Add summary task.

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,16 @@
 var gulp = require('gulp');
 var del = require('del');
 var ghPages = require('gulp-gh-pages');
+var summary = require('gitbook-summary/lib/').summary;
+
+gulp.task('summary', function() {
+    // You can use multiple globbing patterns as you would with `gulp.src`
+    var options = {
+        root: "."
+    };
+
+    return summary(options);
+});
 
 gulp.task('clean', function() {
     // You can use multiple globbing patterns as you would with `gulp.src`
@@ -12,4 +22,3 @@ gulp.task('deploy', function() {
     return gulp.src('./_book/**/*')
         .pipe(ghPages());
 });
-
