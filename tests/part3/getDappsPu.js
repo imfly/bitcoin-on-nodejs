e2be78aeac7ed9cdce01dcf82e588ac735305a3e
imfly <kubying@foxmail.com>
2016-08-23 22:19:02 +0800

    update...

--- tests/part3/getDappsPu.js
+++ tests/part3/getDappsPu.js
@@ -1,24 +1,60 @@
 var fs = require('fs');
 var _ = require('ramda');
+var Task = require('data.task');
 
-var imPure = {
-  read: function(path, cb) {
-    fs.readFile(path, 'utf8', cb);
-  },
-
-  write: _.curry(function(path, cb, data) {
-    fs.writeFile(path, data, cb);
-  })
+// read : String -> Task(Error, Buffer)
+function read(path) {
+    return new Task(function(reject, resolve) {
+        fs.readFile(path, 'utf8', function(error, data) {
+            if (error) reject(error)
+            else resolve(data)
+        })
+    })
 }
 
-var getFile = _.compose(imPure.read, function (file) {
-    return file;
+// decode : Task(Error, Buffer) -> Task(Error, String)
+function decode(buffer) {
+    return buffer.map(function(a) {
+        return a.toString('utf-8')
+    })
+}
+
+var trace = _.curry(function(tag, x) {
+    console.log(tag, x);
+    return x;
 });
 
-var setFile = imPure.write('dapps.pu', console.log('Saved!'));
+var file = function(file) {
+    return file;
+};
 
-var generatePu = _.compose(setFile, getFile)
+var match = _.curry(function(what, str) {
+  console.log(str.match(what));
+    return str.match(what);
+});
 
-generatePu('./dapps.js');
+var privates = match(/privated(.*) = f/g);
 
-console.log('Ok');
+// var filter = _.curry(function(f, txt) {
+//     return txt.match(f);
+// });
+
+// var findAllOf = filter(hasSpaces);
+
+var write =  _.curry(function(path, data, cb) {
+    fs.writeFile(path, privates(data), cb);
+})
+
+var getFile = _.compose(decode, read, file);
+
+getFile('./dapps.js').fork(
+    function(error) {
+        throw error
+    },
+    function(data) {
+        var setFile = write('dapps.pu');
+        setFile(data, function() {
+            console.log('Saved!');
+        });
+    }
+)
